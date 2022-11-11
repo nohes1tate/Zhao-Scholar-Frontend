1c3aca645e1afde40a123f0a0164047e44b85852
陈百铭 <957167412@qq.com>
2022-11-11 23:56:53 +0800

    scholarPage update

--- src/views/ScholarPortal.vue
+++ src/views/ScholarPortal.vue
@@ -1,12 +1,67 @@
 <template>
-  <h1>学者门户页面</h1>
+  <div class="scholar-page">
+    <div class="scholar-info-box box">
+      <div class="scholar-top">
+        <div class="scholar-img">
+          <v-avatar color="grey">
+            <v-icon dark>mdi-account-circle</v-icon>
+          </v-avatar>
+        </div>
+        <div class="scholar-mid-info">
+          <div style="font-size: 30px">
+            Scholar Name
+          </div>
+          <div>
+            <v-icon>mdi-home-outline</v-icon>
+            Beihang University
+          </div>
+        </div>
+      </div>
+      <div class="scholar-bottom"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-
   export default {
     name: 'ScholarPortal',
-
-
   }
 </script>
+
+<style>
+.scholar-page{
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+  padding-left: 10vh;
+}
+.scholar-info-box {
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  width: 100vh;
+}
+.box{
+  box-shadow: 0 2px 10px rgb(0 0 0 / 10%);
+  border-radius: 4px;
+}
+.scholar-top {
+  display: flex;
+  flex-direction: row;
+  min-height: 25vh;
+  align-items: center;
+}
+.scholar-bottom {
+  display: flex;
+  flex-direction: row;
+  background: #f9fafc;
+  height: 20vh;
+}
+.scholar-img {
+  width: 10vh;
+}
+.scholar-mid-info {
+  display: flex;
+  flex-direction: column;
+}
+</style>
