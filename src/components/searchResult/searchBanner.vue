64c56498f216b342cfea6a208b1f0bae25e17916
Ivine11 <i2506851474@163.com>
2022-11-13 18:50:30 +0800

    searchPage

---
+++ src/components/searchResult/searchBanner.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-card
+        width=100%
+        height="200px"
+        style="box-shadow: none;"
+        >
+        
+        </v-card>
+    </div>
+</template>
--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,12 +1,22 @@
 <template>
-  <h1>搜索展示页面</h1>
+  <div>
+   <searchBannerVue></searchBannerVue>
+   <infoCardVue></infoCardVue>
+  </div>
 </template>
 
 <script>
-
+  import infoCardVue from '@/components/searchResult/infoCard.vue';
+  import searchBannerVue from '@/components/searchResult/searchBanner.vue';
   export default {
     name: 'SearchResult',
-
-
+    components:{
+      infoCardVue,
+      searchBannerVue
+    },
+    data:()=>({
+      
+    })
   }
+
 </script>
