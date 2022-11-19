713e3218da7e5562c4031feeaf50e2673f6a13c2
nohesitate <20373441@buaa.edu.cn>
2022-11-20 00:16:42 +0800

    [页面更新]机构门户添加数据卡片

--- src/components/InstitutionPortal/ChartsCard.vue
+++ src/components/InstitutionPortal/ChartsCard.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="institution-chart-card">
-    <v-card height="25vh" width="30vw"  elevation="2">
+    <v-card height="45vh" width="30vw"  elevation="2" @click="nani">
       <div>{{ title }}</div>
+      <div :id='"charts-"+ id' style="height: 100%; width: 100%; cursor: pointer"></div>
     </v-card>
   </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+
 
 export default {
   name: "ChartsCard",
@@ -14,6 +17,58 @@ export default {
     title: {
       type: String,
       default: ""
+    },
+    id: {
+      type: Number,
+      default: -1
+    },
+    xAxis: {
+      type: Array,
+      default (){
+        return []
+      }
+    },
+    data: {
+      type: Array,
+      default (){
+        return []
+      }
+    }
+  },
+
+  mounted() {
+    this.initEcharts()
+  },
+
+  methods: {
+    nani() {
+      console.log("114514")
+    },
+    initEcharts() {
+      console.log("fuck")
+      let chartDom = document.getElementById('charts-' + this.id);
+      let myChart = echarts.init(chartDom);
+      let option;
+      let x = this.xAxis
+      let d = this.data
+
+      option = {
+        xAxis: {
+          type: 'category',
+          data: x
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: d,
+            type: 'bar'
+          }
+        ]
+      };
+
+      option && myChart.setOption(option);
     }
   }
 }
@@ -23,5 +78,7 @@ export default {
 .institution-chart-card {
   margin-left: 2vw;
   margin-top: 2vh;
+  cursor: pointer;
 }
+
 </style>
--- src/views/InstitutionPortal/InstitutionPortal.vue
+++ src/views/InstitutionPortal/InstitutionPortal.vue
@@ -19,8 +19,12 @@
                 <div class="institution-description">{{institutionDescription}}</div>
               </v-row>
               <v-row>
-                <v-btn> button1 </v-btn>
-                <v-btn> button2 </v-btn>
+                <v-col>
+                <v-btn  color="primary"> button1 </v-btn>
+                </v-col>
+                <v-col>
+                <v-btn  color="primary"> button2 </v-btn>
+                </v-col>
               </v-row>
             </v-col>
           </v-row>
@@ -44,7 +48,10 @@
     </div>
     <div class="institution-charts">
       <div v-for="item in charts" v-bind:key="item.id">
-        <ChartsCard :title="item.title"></ChartsCard>
+        <ChartsCard :title="item.title"
+                    :id="item.id"
+        :x-axis="item.xAxis"
+        :data="item.data"></ChartsCard>
       </div>
     </div>
   </div>
@@ -56,10 +63,30 @@ import ChartsCard from "@/components/InstitutionPortal/ChartsCard";
     name: 'SearchResult',
     components: {ChartsCard},
     data: () => ( {
-      charts: [{id: 1,value: 2,title: "12345678910"},
-        {id: 2,value: 6, title: "65498491165"},
-        {id: 3,value: 4, title: "498498818498"},
-        {id: 4,value: 1, title: "48984121548956456"}],
+      charts: [{id: 1,
+        value: 2,
+        type: 0,
+        title: "12345678910",
+        xAxis: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+      data: [1100, 1200, 1250, 1255, 1276, 1305, 1350, 1358, 1422, 1530, 108]},
+        {id: 2,
+          value: 6,
+          type: 0,
+          title: "65498491165",
+          xAxis: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+          data: [1000, 1100, 1150, 1255, 1276, 1605, 1350, 1358, 1947, 1422, 408]},
+        {id: 3,
+          value: 4,
+          type: 1,
+          title: "498498818498",
+          xAxis: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+          data: [2000, 1300, 1250, 1255, 1576, 1305, 1350, 2258, 1422, 1597, 108]},
+        {id: 4,
+          value: 1,
+          type: 1,
+          title: "48984121548956456",
+          xAxis: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+          data: [1100, 1200, 1250, 1255, 1276, 1305, 2350, 1358, 3422, 4321, 108]}],
       institutionLogo: "../../assets/InstitutionLogo/SESC.jpeg",
       institutionDescription: "中国科学院（英文名称：Chinese Academy of Sciences，简称中科院）成立于1949年11月，为中国自然科学最高学术机构、科学技术最高咨询机构、自然科学与高技术综合研究发展中心。 中国科学院提出了建设国家创新体系的构想，先后实施知识创新工程、“创新2020”、《“率先行动”计划暨全面深化改革纲要》，提出了《迎接知识经济时代，建设国家创新体系》《创新促进发展，科技引领未来》《创新2050：科学技术与中国的未来》《科技发展新态势与面向2020年的战略选择》等战略研究报告。 据2021年11月中国科学院官网显示，全院共拥有11个分院、100多家科研院所、3所大学（中国科学院大学、中国科学技术大学，与上海市共建上海科技大学）、130多个国家级重点实验室和工程中心、68个国家野外观测研究站、20个国家科技资源共享服务平台，承担30余项国家重大科技基础设施的建设与运行，正式职工6.9万余人，在学研究生7.9万余人；建成了完整的自然科学学科体系，物理、化学、材料科学、数学、环境与生态学、地球科学等学科整体水平已进入世界先进行列，一些领域方向也具备了进入世界第一方阵的良好态势。在解决关系国家全局和长远发展的重大问题上，已成为不可替代的国家战略科技力量。一批科学家在国家重大科技任务中发挥了关键和中坚作用，并作为我国科技界的代表活跃在国际科技前沿。 [1]  2019年9月，经党中央批准，十九届中央第四轮巡视将对中国科学院党组织开展常规巡视。 [2]",
       papersNum: 114,
@@ -69,7 +96,23 @@ import ChartsCard from "@/components/InstitutionPortal/ChartsCard";
   }
 </script>
 
+
+
 <style scoped>
+::-webkit-scrollbar {
+  width: 5px;
+  height: 16px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgb(241 239 239 / 30%);
+  background-color: #0274b3;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgb(224 223 223 / 30%);
+  border-radius: 10px;
+}
 .institution-top {
   width: 90vw;
   margin-left: 5vw;
