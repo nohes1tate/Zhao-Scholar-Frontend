b896546bc8fa09462ed74490b64b5e8cc62ad231
Ivine11 <i2506851474@163.com>
2022-11-19 18:55:15 +0800

    .

--- src/components/searchResult/infoCard.vue
+++ src/components/searchResult/infoCard.vue
@@ -42,7 +42,7 @@
             Abstract:"We present a variety of new architectural features and training procedures that we apply to the generative adversarial networks (GANs) framework. Using our new techniques, we achieve state-of-the-art results in semi-supervised classification on MNIST, CIFAR-10 and SVHN. The generated images are of high quality as confirmed by a visual Turing test: Our model generates MNIST samples that humans cannot distinguish from real data, and CIFAR-10 samples that yield a human error rate of 21.3%. We also present ImageNet samples with unprecedented resolution and show that our methods enable the model to learn recognizable features of ImageNet classes.",
             pdf:0,//0无，1有
             link:"https://proceedings.neurips.cc/paper/2016/hash/8a3363abe792db2d8761d6403605aeb7-Abstract.html",
-            refernces:123,//被引用数
+            cites:123,//被引用数
             collections:12,//收藏数
             publishTime:2012,//发表时间  
             },
@@ -51,7 +51,7 @@
             Abstract:"We present a variety of new architectural features and training procedures that we apply to the generative adversarial networks (GANs) framework. Using our new techniques, we achieve state-of-the-art results in semi-supervised classification on MNIST, CIFAR-10 and SVHN. The generated images are of high quality as confirmed by a visual Turing test: Our model generates MNIST samples that humans cannot distinguish from real data, and CIFAR-10 samples that yield a human error rate of 21.3%. We also present ImageNet samples with unprecedented resolution and show that our methods enable the model to learn recognizable features of ImageNet classes.",
             pdf:0,//0无，1有
             link:"https://proceedings.neurips.cc/paper/2016/hash/8a3363abe792db2d8761d6403605aeb7-Abstract.html",
-            refernces:123,//被引用数
+            cites:123,//被引用数
             collections:12,//收藏数
             publishTime:2012,//发表时间  
             },
@@ -60,7 +60,7 @@
             Abstract:"Inkjet printing is considered to be a key technology in the field of defined polymer deposition. This article provides an introduction to inkjet printing technology and a short overview of the available instrumentation. Examples of polymer inkjet printing are given, including the manufacturing of multicolor polymer light-emitting diode displays, polymer electronics, three-dimensional printing, and oral dosage forms for controlled drug release. Special emphasis is placed upon the utilized polymers and conditions, such as polymer structure, molar mass, solvents, and concentration. Studies on viscoelastic fluid jets and the formation of viscoelastic droplets under gravity indicate that strain hardening is the key parameter that determines the inkjet printability of polymer solutions.",
             pdf:0,//0无，1有
             link:"https://proceedings.neurips.cc/paper/2016/hash/8a3363abe792db2d8761d6403605aeb7-Abstract.html",
-            refernces:123,//被引用数
+            cites:123,//被引用数
             collections:12,//收藏数
             publishTime:2012,//发表时间  
             }
