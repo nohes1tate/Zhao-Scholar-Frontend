f96d81fad8e434d52eee33182044e169a4da885b
nohesitate <20373441@buaa.edu.cn>
2022-11-21 22:09:51 +0800

    [页面更新]机构门户关系网络卡片添加、图表完善、整体页面滚动条、关系网络预留

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,19 @@ export default {
   }),
 };
 </script>
+<style>
+::-webkit-scrollbar {
+  width: 5px;
+  height: 16px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgb(241 239 239 / 30%);
+  background-color: #0274b3;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgb(224 223 223 / 30%);
+  border-radius: 10px;
+}
+</style>
--- src/components/InstitutionPortal/ChartsCard.vue
+++ src/components/InstitutionPortal/ChartsCard.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="institution-chart-card">
-    <v-card height="45vh" width="30vw"  elevation="2" @click="nani">
+    <v-card height="45vh" width="30vw" elevation="2" @click="showChart" v-if="chartType === 1">
       <div>{{ title }}</div>
       <div :id='"charts-"+ id' style="height: 100%; width: 100%; cursor: pointer"></div>
+    </v-card>
+    <v-card height="45vh" width="30vw" elevation="2" v-else>
+      <div>{{ title }}</div>
+      <div ref="insRelation" style="width: 100%; height: 40vh; cursor: pointer"></div>
     </v-card>
   </div>
 </template>
@@ -14,6 +18,10 @@ import * as echarts from 'echarts'
 export default {
   name: "ChartsCard",
   props: {
+    chartType: {
+      type: Number,
+      default: 1
+    },
     title: {
       type: String,
       default: ""
@@ -28,29 +36,78 @@ export default {
         return []
       }
     },
-    data: {
+    yData: {
       type: Array,
       default (){
         return []
+      }
+    },
+    graph: {
+      type: Object,
+      default () {
+        return null
       }
     }
   },
 
   mounted() {
-    this.initEcharts()
+    if (this.chartType===1)
+      this.initEcharts()
+    else {
+      this.initRelation()
+    }
   },
 
   methods: {
-    nani() {
-      console.log("114514")
+    showChart() {
+      this.$emit("showChart",this.xAxis, this.yData, this.title)
+    },
+    initRelation() {
+      var myChart = this.$echarts.init(this.$refs.insRelation)
+      var option = {
+        tooltip: {},
+        legend: [
+          {
+            data: this.graph.categories.map(function (a) {
+              return a.name;
+            })
+          }
+        ],
+        series: [
+          {
+            type: 'graph',
+            layout: 'none',
+            data: this.graph.nodes,
+            links: this.graph.links,
+            categories: this.graph.categories,
+            roam: true,
+            label: {
+              show: true,
+              position: 'right',
+              formatter: '{b}'
+            },
+            labelLayout: {
+              hideOverlap: true
+            },
+            scaleLimit: {
+              min: 0.4,
+              max: 2
+            },
+            lineStyle: {
+              color: 'source',
+              curveness: 0.3
+            }
+          }
+        ]
+      };
+      myChart.setOption(option);
     },
     initEcharts() {
-      console.log("fuck")
       let chartDom = document.getElementById('charts-' + this.id);
       let myChart = echarts.init(chartDom);
       let option;
       let x = this.xAxis
-      let d = this.data
+      let d = this.yData
 
       option = {
         xAxis: {
@@ -77,7 +134,7 @@ export default {
 <style scoped>
 .institution-chart-card {
   margin-left: 2vw;
-  margin-top: 2vh;
+  margin-top: 3vh;
   cursor: pointer;
 }
 
