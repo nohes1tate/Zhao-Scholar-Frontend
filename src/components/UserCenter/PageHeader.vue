9317445c03130e651ef33574a7e83f1a5786f8ab
Ivine11 <i2506851474@163.com>
2022-12-10 18:31:31 +0800

    debug

--- src/components/UserCenter/PageHeader.vue
+++ src/components/UserCenter/PageHeader.vue
@@ -88,6 +88,7 @@ export default {
     searchTag() {
       if (this.search === "") return false;
       console.log(this.search);
+      this.$router.push({path:'/search', query:{keyword:this.search}})
     },
     goToLogin(){
       this.$router.push('/login')
