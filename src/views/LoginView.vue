9c592d4da2c069f086f3045b697a265da7dbbe44
cYw <1147701986@qq.com>
2022-12-10 16:02:03 +0800

    登录界面收工

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,6 +17,7 @@
             height=10
             color="black"
             style="width: 20vw;margin-top: 20px;"
+            :rules="[rules.username]"
         ></v-text-field>
         <v-text-field
             v-model="email"
@@ -27,6 +28,7 @@
             height=10
             color="black"
             style="width: 20vw;"
+            :rules="[rules.email]"
         ></v-text-field>
         <v-text-field
             v-model="password1"
@@ -37,6 +39,10 @@
             height=10
             color="black"
             style="width: 20vw;"
+            :rules="[rules.password]"
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show2 ? 'text' : 'password'"
+            @click:append="show2 = !show2"
         ></v-text-field>
         <div style="display: flex;flex-direction: row">
           <v-text-field
@@ -48,9 +54,13 @@
               height=10
               color="black"
               style="width: 20vw;"
+              :rules="[rules.checkPassword2]"
+              :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show3 ? 'text' : 'password'"
+              @click:append="show3 = !show3"
           ></v-text-field>
         </div>
-        <button @click.prevent="register">注册</button>
+        <button class="button" @click.prevent="register">注册</button>
       </form>
     </div>
 
@@ -78,9 +88,68 @@
             height=10
             color="black"
             style="width: 20vw;"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            @click:append="show1 = !show1"
         ></v-text-field>
-        <a class="forget">忘记密码？</a>
-        <button @click.prevent="login">登录</button>
+        <v-dialog
+            v-model="forgetPasswordDialog"
+            persistent
+            max-width="600px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <a class="forget" v-bind="attrs" v-on="on">忘记密码？</a>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">修改密码</span>
+            </v-card-title>
+                <v-text-field
+                    v-model="forgetEmail"
+                    label="邮箱"
+                    required
+                    color="black"
+                    style="width: 80%;margin: auto;padding-top: 20px;"
+                ></v-text-field>
+            <v-text-field
+                v-model="newPassword"
+                label="新密码"
+                required
+                color="black"
+                style="width: 80%;margin: auto;padding-top: 20px;"
+            ></v-text-field>
+            <div style="display: flex;flex-direction: row;width: 80%;margin: auto">
+              <v-text-field
+                  v-model="checkCode"
+                  label="验证码"
+                  required
+                  color="black"
+                  style="width: 40%;margin: auto;padding-top: 20px;"
+              ></v-text-field>
+              <v-btn outlined style="margin-left: 20px;margin-top: 10px" @click="forget">
+                发送验证码
+              </v-btn>
+            </div>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="forgetPasswordDialog = false"
+              >
+                取消
+              </v-btn>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="update"
+              >
+                修改密码
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <button class="button" @click.prevent="login">登录</button>
       </form>
     </div>
 
@@ -94,7 +163,7 @@
           <p>
             点击登录按钮来继续你的学术分享之旅
           </p>
-          <button class="ghost" id="signIn">登录</button>
+          <button class="button ghost" id="signIn">登录</button>
         </div>
 
         <!-- 覆盖右边 -->
@@ -103,7 +172,7 @@
           <p>
             想要加入我们？点击注册按钮开启你的学术分享之旅
           </p>
-          <button class="ghost" id="signUp">注册</button>
+          <button class="button ghost" id="signUp">注册</button>
         </div>
       </div>
     </div>
@@ -117,18 +186,141 @@ export default {
   name: "LoginView",
   data(){
     return{
+      forgetPasswordDialog:false,
+      show1:false,
+      show2:false,
+      show3:false,
       username:"",
       email:"",
       password:"",
       password1:"",
       password2:"",
       confirmCode:"",
+      forgetEmail:"",
+      newPassword:"",
+      checkCode:"",
+      usernameCheck:false,
+      emailCheck:false,
+      passwordCheck:false,
+      passwordConfirmCheck:false,
+      rules: {
+        username: value =>{
+          const reg=/^[\u4e00-\u9fa5_a-zA-Z0-9]+$/;
+          if (!value) {
+            return '用户名不能为空'
+          } else if (!reg.test(value)) {
+            return '用户名由中英文、数字或下划线组成'
+          } else {
+            this.usernameCheck=true;
+            return reg.test(value)
+          }
+        },
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          if(!pattern.test(value))
+            return '请输入有效邮箱格式'
+          else{
+            this.emailCheck=true;
+            return pattern.test(value)
+          }
+        },
+        password: value =>{
+          var reg_pwd=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,18}$/;
+          if(!value){
+            return '请输入密码'
+          }
+          else if(!reg_pwd.test(value)){
+            return '密码至少同时包含字母和数字，且长度为8-18'
+          }
+          else{
+            this.passwordCheck=true;
+            return reg_pwd.test(value)
+          }
+        },
+        checkPassword2: value =>{
+          if (value === '') {
+            return  '请再次输入密码';
+          } else if (value !== this.password1) {
+            return '两次输入密码不一致!';
+          }
+          else {
+            this.passwordConfirmCheck=true;
+            return true
+          }
+        },
+      },
     }
   },
   methods:{
-    test(){
-      console.log("login");
+    forget(){
+      const self = this;
+      const formData = new FormData();
+      formData.append("email", self.forgetEmail);
+      self.$axios({
+        method: 'post',
+        url: 'UserManager/forget/',
+        data: formData,
+      })
+          .then(res => {
+            switch (res.data.error) {
+              case 0:
+                // 前端保存用户信息
+                this.$message.success("验证码已发送，请前往邮箱确认！");
+                break;
+              case 3001:
+                this.$message.error('表单信息错误（未全部填写或数据类型有误）！');
+                break;
+              case 4001:
+                this.$message.warning('邮件发送失败！');
+                break;
+              case 4002:
+                this.$message.error('邮箱未注册！');
+                break;
+              case 2001:
+                this.$message.error('请求方式错误！');
+                break;
+            }
+          })
     },
+    update(){
+      const self = this;
+      const formData = new FormData();
+      formData.append("email", self.forgetEmail);
+      formData.append("password",self.newPassword);
+      formData.append("code",self.confirmCode);
+      //console.log(self.code)
+      self.$axios({
+        method: 'post',
+        url: 'UserManager/update/',
+        data: formData,
+      })
+          .then(res => {
+            // console.log(res.data.error)
+            // console.log(res.data.msg)
+            switch (res.data.error) {
+              case 0:
+                // 前端保存用户信息
+                this.$message.success("密码修改成功！");
+                this.$router.push('/login');
+                break;
+              case 4001:
+                this.$message.error('密码违规！');
+                break;
+              case 4002:
+                this.$message.error('验证码错误！');
+                break;
+              case 2001:
+                this.$message.error('请求方式错误！');
+                break;
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          })
+    },
+    // test(){
+    //   console.log("login");
+    // },
     login() {
       const formData = new FormData();
       formData.append("username", this.username);
@@ -169,42 +361,44 @@ export default {
           })
     },
     register() {
-      const _formData = new FormData();
-      _formData.append("username", this.username);
-      _formData.append("password1", this.password1);
-      _formData.append("password2", this.password2);
-      _formData.append("email", this.email);
-      this.$axios({
-        method: 'post',
-        url: 'UserManager/register/',
-        data: _formData,
-      })
-          .then(res => {
-            switch (res.data.status) {
-              case 200:
-                this.$message.success("注册成功！");
-                setTimeout(() => {
-                  this.$router.push("/login");
-                }, 1000);
-                break;
-              case 401:
-                this.$message.warning("请勿重复注册");
-                break;
-              case 402:
-                this.$message.error("验证码错误");
-                break;
-              case 404:
-                this.$message.error("查无注册用户信息，请重新注册");
-                break;
-              case 600:
-                this.$message.error("验证码失效，请重新获取");
-                break;
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          })
-    },
+      if(this.emailCheck&&this.usernameCheck&&this.passwordCheck&&this.passwordConfirmCheck) {
+        const _formData = new FormData();
+        _formData.append("username", this.username);
+        _formData.append("password1", this.password1);
+        _formData.append("password2", this.password2);
+        _formData.append("email", this.email);
+        this.$axios({
+          method: 'post',
+          url: 'UserManager/register/',
+          data: _formData,
+        })
+            .then(res => {
+              console.log(res.data.error)
+              switch (res.data.error) {
+                case 200:
+                  this.$message.success("注册成功！");
+                  setTimeout(() => {
+                    this.$router.push("/login");
+                  }, 1000);
+                  break;
+                case 4002:
+                  this.$message.warning("请勿重复注册");
+                  break;
+                case 404:
+                  this.$message.error("查无注册用户信息，请重新注册");
+                  break;
+                case 600:
+                  this.$message.error("验证码失效，请重新获取");
+                  break;
+              }
+            })
+            .catch(err => {
+              console.log(err);
+            })
+      }
+      else
+        this.$message.error("请填写正确的表单");
+    }
   },
   mounted(){
     const container = document.getElementById('container');
@@ -219,6 +413,12 @@ export default {
       container.classList.remove('penal-right-active');
     }
   },
+  created() {
+    this.usernameCheck=false;
+    this.emailCheck=false;
+    this.passwordCheck=false;
+    this.passwordConfirmCheck=false;
+  }
 }
 </script>
 
@@ -236,11 +436,11 @@ export default {
   height: 100vh;
   background-color: #F6F5F7;
 }
-button {
+.button {
   outline: none;
-  border: 1px solid #FF4B2B;
+  border: 1px solid #1E88E5;
   border-radius: 20px;
-  background-color: #FF4B2B;
+  background-color: #039BE5;
   padding: 10px 40px;
   font-size: 12px;
   text-transform: uppercase;
@@ -331,7 +531,7 @@ button:active {
   right: 0;
   width: 50%;
   height: 100%;
-  background-color: tomato;
+  background-color: #0288D1;
   overflow: hidden;
   transition: transform .65s ease-in-out;
   z-index: 100;
@@ -342,7 +542,7 @@ button:active {
   left: -100%;
   width: 200%;
   height: 100%;
-  background: linear-gradient(to right, #FF4B2D, #FF426B);
+  background: linear-gradient(to right, #1E88E5, #0288D1);
 }
 
 .overlay-penal {
@@ -365,7 +565,7 @@ button:active {
   text-transform: capitalize;
 }
 .forget:hover {
-  border-bottom: 2px tomato solid;
+  border-bottom: 2px #0288D1 solid;
 }
 
 .overlay-left-container {
