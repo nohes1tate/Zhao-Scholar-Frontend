e7abbc528143205c4eedea2c76ae15d7e1845ca7
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-12-12 00:21:37 +0800

    主页CSS基本完工

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div height="100vh">
-    <div width="100vw" class="bgimg" :style="{ height: '60vh' }">
+    <div width="100vw" class="bgimg" :style="{ 'min-height': '60vh' }">
       <!-- <v-card   background="linear-gradient(red, blue)"> -->
-      <v-row :style="{ height: '60vh' }">
-        <v-col cols="12" md="12" sm="2">
+      <v-row :style="{ height: '80vh' }">
+        <v-col cols="12" md="12" sm="1">
           <v-row>
             <v-col cols="12" md="2" sm="12">
               <h1 class="d-flex justify-center" :style="{ color: 'white','margin-left':'0px'}">
-                <img height="100px" :style="{'margin-left':'50px'}" src="../assets/HatchfulExport-All/twitter_header_photo_2.png"/>
+                <img height="100px" :style="{'margin-left':'50px'}" src="../assets/HatchfulExport-All/logo_transparent.png"/>
               </h1>
             </v-col>
             <v-col cols="12" md="7" sm="12" />
@@ -23,7 +23,7 @@
             </v-col>
           </v-row>
         </v-col>
-        <v-col cols="12" md="12" sm="10">
+        <v-col cols="12" md="12" sm="6" :style="{'margin-top':'-100px'}">
           <v-row>
             <v-col cols="12" md="3" sm="12" />
             <v-col cols="12" md="6" sm="12">
@@ -66,28 +66,144 @@
             <v-col cols="12" md="3" sm="12" />
           </v-row>
         </v-col>
-        <v-col cols="12" md="12" sm="2" />
+        <v-col cols="12" md="12" sm="3" />
+        <v-col cols="12" md="12" sm="1" :style="{'max-height':'150px'}">
+        <v-row>
+     
+      <v-col cols="12" md="1" sm="12"/>
+      <v-col cols="12" md="10" sm="12" >
+        <div :style="{'opacity': '0.8'}">
+          <v-card text :style="{'background-color':'#333333',color:'white',height:'100px'}">
+            <v-row>
+              <v-col>
+                <v-row>
+                  <v-col cols="12" md="3" sm="12" class="d-flex align-center d-flex justify-center">
+                    <v-icon class="material-symbols-outlined" :style="{color:'white','font-size':'40px','margin-left':'5px'}">school</v-icon>
+                  </v-col>
+                  <v-col cols="12" md="9" sm="12" :style="{padding:'0px'}">
+                    <v-row>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-bottom':'5px'}">
+                    <h3 :style="{color:'#CCCCCC','margin-top':'5px'}">Authors</h3>
+                  </v-col>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-top':'0px'}">
+                    <h1>{{general.authors}}</h1>
+                  </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+             <v-col>
+                <v-row>
+                  <v-col cols="12" md="3" sm="12" class="d-flex align-center d-flex justify-center">
+                    <v-icon class="material-symbols-outlined" :style="{color:'white','font-size':'40px','margin-left':'5px'}">description</v-icon>
+                  </v-col>
+                  <v-col cols="12" md="9" sm="12" :style="{padding:'0px'}">
+                    <v-row>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-bottom':'5px'}">
+                    <h3 :style="{color:'#CCCCCC','margin-top':'5px'}">Papers</h3>
+                  </v-col>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-top':'0px'}">
+                    <h1>{{general.papers}}</h1>
+                  </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col>
+                <v-row>
+                  <v-col cols="12" md="3" sm="12" class="d-flex align-center d-flex justify-center">
+                    <v-icon class="material-symbols-outlined" :style="{color:'white','font-size':'40px','margin-left':'5px'}">domain</v-icon>
+                  </v-col>
+                  <v-col cols="12" md="9" sm="12" :style="{padding:'0px'}">
+                    <v-row>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-bottom':'5px'}">
+                    <h3 :style="{color:'#CCCCCC','margin-top':'5px'}">Affiliations</h3>
+                  </v-col>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-top':'0px'}">
+                    <h1>{{general.affiliations}}</h1>
+                  </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col>
+                <v-row>
+                  <v-col cols="12" md="3" sm="12" class="d-flex align-center d-flex justify-center">
+                    <v-icon class="material-symbols-outlined" :style="{color:'white','font-size':'40px','margin-left':'5px'}">meeting_room</v-icon>
+                  </v-col>
+                  <v-col cols="12" md="9" sm="12" :style="{padding:'0px'}">
+                    <v-row>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-bottom':'5px'}">
+                    <h3 :style="{color:'#CCCCCC','margin-top':'5px'}">Venues</h3>
+                  </v-col>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-top':'0px'}">
+                    <h1>{{general.venues}}</h1>
+                  </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col>
+                <v-row>
+                  <v-col cols="12" md="3" sm="12" class="d-flex align-center d-flex justify-center">
+                    <v-icon class="material-symbols-outlined" :style="{color:'white','font-size':'40px','margin-left':'5px'}">pie_chart</v-icon>
+                  </v-col>
+                  <v-col cols="12" md="9" sm="12" :style="{padding:'0px'}">
+                    <v-row>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-bottom':'5px'}">
+                    <h3 :style="{color:'#CCCCCC','margin-top':'5px'}">Field</h3>
+                  </v-col>
+                  <v-col cols="12" md="12" sm="6" :style="{'padding-top':'0px'}">
+                    <h1>{{general.fields}}</h1>
+                  </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card>
+        </div>
+      </v-col>
+      <v-col cols="12" md="1" sm="12"/>
+    </v-row>
+        </v-col>
+        <v-col cols="12" md="12" sm="1" />
       </v-row>
     </div>
-    <div
-      :style="{
+    <div :style="{
         'background-color': '#EEEEEE',
         'min-height': '50vh',
         'padding-bottom': '50px',
-      }"
-    >
+      }">
       <v-row>
         <v-col cols="12" md="4" sm="12">
           <v-card
-            :style="{ 'margin-top': '-50px' }"
+          :style="{'margin-top':'-80px'}"
             class="mx-auto"
             max-width="300"
             tile
           >
+          <v-card color="#e1e7e7" 
+          elevation=0
+          :style="{'border-radius':'0px'}">
+              <v-row>
+                <v-col cols="12" md="1" sm="12" />
+                <v-col cols="12" md="2" sm="12" class="d-flex justify-center">
+              <v-list-item-avatar color="white" size="60" :style="{'margin-left':'20px'}">
+                <v-icon class="material-symbols-outlined" :style="{color:'#809def','font-size': '40px'}">
+              person
+              </v-icon>
+              </v-list-item-avatar>
+                </v-col>
+                <v-col cols="12" md="9" sm="12">
+            <v-card-title :style="{'color':'black'}">
+              Top Author</v-card-title>
+                </v-col>
+              </v-row>
+            </v-card>
+
+
             <v-list dense>
-              <v-subheader :style="{ 'font-size': '20px' }"
-                >Scholars</v-subheader
-              >
               <v-list-item-group v-model="item" color="primary">
                 <v-list-item
                   v-for="(item, i) in TopAuthors"
@@ -119,10 +235,156 @@
                 </v-list-item>
               </v-list-item-group>
             </v-list>
+            <div class="d-flex justify-center">
+           <v-btn icon color="black" :disabled="page1==1" @click="page1=page1-1">
+              <v-icon>navigate_before</v-icon>
+            </v-btn>
+            <v-btn icon color="black" :disabled="page1==4" @click="page1=page1+1">
+              <v-icon>navigate_next</v-icon>
+            </v-btn>
+            </div>
           </v-card>
         </v-col>
-        <v-col cols="12" md="4" sm="12"> </v-col>
-        <v-col cols="12" md="4" sm="12"> </v-col>
+        <v-col cols="12" md="4" sm="12">
+        <v-card
+          :style="{'margin-top':'-80px'}"
+            class="mx-auto"
+            max-width="300"
+            tile
+          >
+          <v-card color="#e1e7e7" 
+          elevation=0
+          :style="{'border-radius':'0px'}">
+              <v-row>
+                <v-col cols="12" md="1" sm="12" />
+                <v-col cols="12" md="2" sm="12" class="d-flex justify-center">
+              <v-list-item-avatar color="white" size="60" :style="{'margin-left':'20px'}">
+                <v-icon class="material-symbols-outlined" :style="{color:'#809def','font-size': '40px'}">
+              apartment
+              </v-icon>
+              </v-list-item-avatar>
+                </v-col>
+                <v-col cols="12" md="9" sm="12">
+            <v-card-title :style="{'color':'black'}">
+              Top Affiliations</v-card-title>
+                </v-col>
+              </v-row>
+            </v-card>
+
+
+            <v-list dense>
+              <v-list-item-group v-model="item" color="primary">
+                <v-list-item
+                  v-for="(item, i) in TopAffiliations"
+                  :key="i"
+                  :style="{ 'margin-left': '10px', 'margin-right': '10px' }"
+                  inactive
+                >
+                  <v-row>
+                    <v-col
+                      class="d-flex justify-space-between"
+                      :style="{ 'padding-top': '0px', 'padding-bottom': '0px' }"
+                    >
+                      <div class="d-flex justify-space-between">
+                        {{ item.name }}
+                      </div>
+                      <div class="d-flex justify-end">
+                        {{ item.progress }} papers
+                      </div>
+                    </v-col>
+
+                    <v-progress-linear
+                      v-model="item.percentage"
+                      color="primary"
+                      rounded
+                      height="8"
+                      background-opacity="0.1"
+                    ></v-progress-linear>
+                  </v-row>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+            <div class="d-flex justify-center">
+           <v-btn icon color="black" :disabled="page2==1" @click="page1=page2-1">
+              <v-icon>navigate_before</v-icon>
+            </v-btn>
+            <v-btn icon color="black" :disabled="page2==4" @click="page1=page2+1">
+              <v-icon>navigate_next</v-icon>
+            </v-btn>
+            </div>
+          </v-card>
+
+        </v-col>
+        <v-col cols="12" md="4" sm="12">
+          <v-card
+          :style="{'margin-top':'-80px'}"
+            class="mx-auto"
+            max-width="300"
+            tile
+          >
+          <v-card color="#e1e7e7" 
+          elevation=0
+          :style="{'border-radius':'0px'}">
+              <v-row>
+                <v-col cols="12" md="1" sm="12" />
+                <v-col cols="12" md="2" sm="12" class="d-flex justify-center">
+              <v-list-item-avatar color="white" size="60" :style="{'margin-left':'20px'}">
+                <v-icon class="material-symbols-outlined" :style="{color:'#809def','font-size': '40px'}">
+              pages
+              </v-icon>
+              </v-list-item-avatar>
+                </v-col>
+                <v-col cols="12" md="9" sm="12">
+            <v-card-title :style="{'color':'black'}">
+              Top Journals</v-card-title>
+                </v-col>
+              </v-row>
+            </v-card>
+
+
+            <v-list dense>
+              <v-list-item-group v-model="item" color="primary">
+                <v-list-item
+                  v-for="(item, i) in TopJournals"
+                  :key="i"
+                  :style="{ 'margin-left': '10px', 'margin-right': '10px' }"
+                  inactive
+                >
+                  <v-row>
+                    <v-col
+                      class="d-flex justify-space-between"
+                      :style="{ 'padding-top': '0px', 'padding-bottom': '0px' }"
+                    >
+                      <div class="d-flex justify-space-between">
+                        {{ item.name }}
+                      </div>
+                      <div class="d-flex justify-end">
+                        {{ item.progress }} papers
+                      </div>
+                    </v-col>
+
+                    <v-progress-linear
+                      v-model="item.percentage"
+                      color="primary"
+                      rounded
+                      height="8"
+                      background-opacity="0.1"
+                    ></v-progress-linear>
+                  </v-row>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+            <div class="d-flex justify-center">
+           <v-btn icon color="black" :disabled="page3==1" @click="page1=page3-1">
+              <v-icon>navigate_before</v-icon>
+            </v-btn>
+            <v-btn icon color="black" :disabled="page3==4" @click="page1=page3+1">
+              <v-icon>navigate_next</v-icon>
+            </v-btn>
+            </div>
+          </v-card>
+        </v-col>
+        
       </v-row>
     </div>
     <div :style="{ 'background-color': '#FFFFFF', 'min-height': '50vh' }">
@@ -164,15 +426,50 @@
 
 <script>
 import user from "@/store/user"
+import request from "@/utils/request";
 export default {
   name: "HomeView",
   data() {
     return {
       search: "",
+      page1:1,
+      page2:1,
+      page3:1,
       state:user.getters.getUser(user.state),
       showSelect:false,
+      general:{
+        authors:2147483647,
+        papers:114514000,
+        affiliations:1919810,
+        venues:20373614,
+        fields:9876543,
+      },
       items:[{key: '1234',name: '1234'},],
       TopAuthors: [
+        { progress: 454, name: "AAA", percentage: 100 },
+        { progress: 453, name: "BBB", percentage: 453 / 4.54 },
+        { progress: 346, name: "CCC", percentage: 346 / 4.54 },
+        { progress: 234, name: "DDD", percentage: 234 / 4.54 },
+        { progress: 220, name: "EEE", percentage: 220 / 4.54 },
+        { progress: 198, name: "FFF", percentage: 198 / 4.54 },
+        { progress: 176, name: "GGG", percentage: 176 / 4.54 },
+        { progress: 154, name: "HHH", percentage: 154 / 4.54 },
+        { progress: 132, name: "III", percentage: 132 / 4.54 },
+        { progress: 123, name: "JJJ", percentage: 123 / 4.54 },
+      ],
+      TopAffiliations: [
+        { progress: 454, name: "AAA", percentage: 100 },
+        { progress: 453, name: "BBB", percentage: 453 / 4.54 },
+        { progress: 346, name: "CCC", percentage: 346 / 4.54 },
+        { progress: 234, name: "DDD", percentage: 234 / 4.54 },
+        { progress: 220, name: "EEE", percentage: 220 / 4.54 },
+        { progress: 198, name: "FFF", percentage: 198 / 4.54 },
+        { progress: 176, name: "GGG", percentage: 176 / 4.54 },
+        { progress: 154, name: "HHH", percentage: 154 / 4.54 },
+        { progress: 132, name: "III", percentage: 132 / 4.54 },
+        { progress: 123, name: "JJJ", percentage: 123 / 4.54 },
+      ],
+      TopJournals: [
         { progress: 454, name: "AAA", percentage: 100 },
         { progress: 453, name: "BBB", percentage: 453 / 4.54 },
         { progress: 346, name: "CCC", percentage: 346 / 4.54 },
@@ -221,10 +518,23 @@ export default {
         {key: 'ccc',name: 'ccc',type:''},
         {key: 'ccc',name: 'ccc',type:''}]
     },
+    getAll(){
+      let data=new FormData();
+      request('GET', "/api/PortalManager/getGeneralInfo/",data)
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   },
   watch: {
     search:'inputHandle'
   },
+  mounted(){
+    this.getAll()
+  }
 };
 </script>
 <style scoped>
